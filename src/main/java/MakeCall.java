b06a0a1df3c459d73b0a80f51a43b2cc52c63b90
Msarangi <50727426+Msarangi@users.noreply.github.com>
2019-08-13 10:48:32 +0530

    Removed auth token

--- src/main/java/MakeCall.java
+++ src/main/java/MakeCall.java
@@ -8,8 +8,8 @@ import com.twilio.type.PhoneNumber;
 
 public class MakeCall {
     // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = "AC8f66f031fd4cc49c090d4a6f93731357";
-    public static final String AUTH_TOKEN = "9dc63e1919aa6b1a4854a08328723109";
+    public static final String ACCOUNT_SID = "";
+    public static final String AUTH_TOKEN = "";
 
     public static void main(String[] args) throws URISyntaxException {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
